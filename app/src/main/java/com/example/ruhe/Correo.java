9da596138c946eb5db1df22295e075e31f83937b
Tatiana <aacelas@unal.edu.co>
2023-06-21 23:59:01 -0500

    ESOOOOOOO PATRONAAAAAA

--- app/src/main/java/com/example/ruhe/Correo.java
+++ app/src/main/java/com/example/ruhe/Correo.java
@@ -22,13 +22,12 @@ public class Correo extends AppCompatActivity {
     private static Session session;
     private static String correo = "ruheaplicacion@gmail.com";
     private static String contra = "gbqdomxhuovibywj";
-
-    private static String ubicacion = "ubicacion";
-    private static String contacto;
+    private static String contacto, destinoUsuario, ubicacion;
 
     public static void enviarCorreo(String ubicacion){
         ArrayList<Ruta> rutas= new ArrayList<>(MainActivity.getRutas());
         contacto=rutas.get(Ingresado.getIndex()).getContacto();
+        destinoUsuario=rutas.get(Ingresado.getIndex()).getDestino();
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
         Properties properties = new Properties();
@@ -52,8 +51,10 @@ public class Correo extends AppCompatActivity {
                 message.setSubject("EMERGENCIA: SU CONTACTO NO RESPONDE");
                 message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(contacto));
                 String url = "https://www.google.com/maps?q="+ubicacion;
-                String clickableLink = "<a href=\"" + url + "\">" + url + "</a>";
-                message.setContent("Hola, somos de la aplicación Ruhe, el envío de este mensaje se debe a que una persona que lo seleccionó a usted como contacto de emergencia, se encuentra en peligro, esta es la ubicación de la persona: \n"+clickableLink, "text/html; charset=utf-8");
+                String LinkUbicacion = "<a href=\"" + url + "\">" + url + "</a>";
+                String url2 = "https://www.google.com/maps?q="+destinoUsuario;
+                String LinkDestino = "<a href=\"" + url2 + "\">" + url2 + "</a>";
+                message.setContent("Hola, somos de la aplicación Ruhe, el envío de este mensaje se debe a que una persona que lo seleccionó a usted como contacto de emergencia, se encuentra en peligro, esta es la ubicación de la persona: \n"+LinkUbicacion+"\nY se dirigía a estas coordenadas: \n"+LinkDestino, "text/html; charset=utf-8");
 
                 Transport.send(message);
             }
